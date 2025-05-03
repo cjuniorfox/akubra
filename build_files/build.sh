b58ca7d5a6c0405d025915d4c1d05c3eef8722e4
Junior_FOX <cjuniorfox@gmail.com>
2025-05-03 10:16:32 -0300

    First commit of akubra based on hyprland-atomic

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,62 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 group install -y \
+	admin-tools \
+	core \
+	fonts \
+	hardware-support \
+	multimedia \
+	standard
+
+dnf5 -y install \
+    adwaita-gtk2-theme \
+    adwaita-icon-theme \
+    bash-completion \
+    blueman \
+    breeze-cursor-theme \
+    breeze-gtk \
+    breeze-icon-theme \
+    chrony \
+    distrobox \
+    firewall-config \
+    flatpak \
+    fontawesome-6-free-fonts \
+    fontawesome-6-brands-fonts \
+    git \
+    gnome-disk-utility \
+    gnome-keyring \
+    gnome-packagekit-installer \
+    gnome-software \
+    gvfs-smb \
+    gvfs-nfs \
+    htop \
+    joystick-support \
+    kitty \
+    liberation-fonts \
+    lxqt-policykit \
+    nautilus \
+    network-manager-applet \
+    pavucontrol \
+    pulseaudio-utils \
+    rofi-wayland \
+    rofimoji \
+    sddm \
+    seahorse \
+    setroubleshoot \
+    socat \
+    swaync \
+    system-config-printer \
+    polkit \
+    tldr \
+    xdg-user-dirs \
+    xdg-user-dirs-gtk \
+    vulkan-headers \
+    vulkan-loader \
+    vulkan-tools \
+    wlr-randr \
+    yaru-{gtk2,gtk3,gtk4,icon,sound}-theme \
+    wl-clipboard
 
 # Use a COPR Example:
 #
@@ -19,6 +73,29 @@ dnf5 install -y tmux
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
+dnf5 -y copr enable solopasha/hyprland
+dnf5 -y copr enable cjuniorfox/hyprland-shell
+
+# Packages from Solopasha's COPR
+dnf5 -y install \
+    cliphist \
+    eww-git \
+    hyprlock \
+    hypridle \
+    hyprland \
+    hyprland-qtutils \
+    hyprpaper hyprshot \
+    xdg-desktop-portal-hyprland
+    
+# Packages from cjuniorfox's COPR
+dnf5 -y install \
+     bibata-cursor-theme \
+     hyprland-shell-config \
+     wol-changer
+
+dnf5 -y copr disable cjuniorfox/hyprland-shell
+dnf5 -y copr disable solopasha/hyprland
+
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+systemctl enable chronyd
